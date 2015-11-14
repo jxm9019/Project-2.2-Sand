ea1b481f6a0ef9425a58d9162ba83e38fb5c5260
Jean Luis Urena <ju7847@rit.edu>
2015-11-13 22:11:13 -0500

    Fixed inital Jake's code. Added comments

--- Project 3/src/Bucket.java
+++ Project 3/src/Bucket.java
@@ -1,52 +1,72 @@
 /**
- * Created by Jacob on 11/13/2015.
+ * @author Jean Luis Urena ju7847 
+ * @author Jake Madlem     jxm9019
+ * CS For Transfers
+ * Project3
+ *
+ * This is a Bucket class. This class is to be used with Sand puzzle, Bucket is an object 
+ * that holds sand, transfers to other Bucket objects, and throws out its sand.
  */
 public class Bucket {
-    public static final int MINSAND = 0;
-    private int maxSand;
-    private int curSand;
+    public static final int MINSAND = 0;      //Minimum amount of sand
+    private int maxSand;                      //Maximum amount of sand able to store in bucket
+    private int curSand;                      //Current amount of sand in bucket
 
+    
+    /* Constructor takes in the maximum amount of sand bucket can take
+     * @param int: amount of sand bucket can take
+     */
     public Bucket(int maxSand){
         this.maxSand = maxSand;
         this.curSand = 0;
     }
 
+    //Copy constructor of bucket
     public Bucket(Bucket bucket){
         this.maxSand = bucket.maxSand;
         this.curSand = bucket.curSand;
     }
 
+    //Getter of current sand
     public int getCurSand() {
         return curSand;
     }
 
+    //Getter of maxSand
     public int getMaxSand() {
         return maxSand;
     }
 
-    public void setMaxSand(int maxSand) {
-        this.maxSand = maxSand;
-    }
-
+    //Sets current amount of sand 
     public void setCurSand(int curSand) {
         this.curSand = curSand;
     }
 
-    public boolean isFull(Bucket bucket){
-        return(bucket.curSand==bucket.maxSand);
+    /* Checks if bucket is full.
+     * @return boolean: true if bucket is full, false otherwise.
+     */
+    public boolean isFull(){
+        return (curSand == maxSand);
     }
 
-    public boolean isEmpty(Bucket bucket){
-        return(bucket.curSand == 0);
+    /* Checks if bucket is empty
+     * @return boolean : true if bucket is empty, false otherwise
+     */
+    public boolean isEmpty(){
+        return(curSand == 0);
     }
 
-    public void emptyBucket(Bucket bucket){
-        bucket.setCurSand(0);
+    /* Empties out a bucket of its sand
+     */
+    public void emptyBucket(){
+        curSand = 0;
         System.out.println("Bucket is now empty.");
     }
 
-    public void fill(Bucket bucket){
-        bucket.setCurSand(maxSand);
+    /* Fills a bucket with its max value of sand it can hold
+     */
+    public void fill(){
+        setCurSand(maxSand);
     }
 
 
--- Project 3/src/Puzzle.java
+++ Project 3/src/Puzzle.java
@@ -1,26 +1,9 @@
 import java.util.ArrayList;
-
-/*
- * Puzzle.java
- *
- * Version:
- * $Id: Puzzle.java,v 1.2 2015/10/22 23:55:05 ju7847 Exp $
- * Revisions:
- * $Log: Puzzle.java,v $
- * Revision 1.2  2015/10/22 23:55:05  ju7847
- * Finished project.
- * Tested thoroughly.
- *
- * Revision 1.1  2015/10/22 04:03:48  ju7847
- * Implemented Puzzle, Mobius and almost finished solver
- *
- */
-
 /**
  * @author Jean Luis Urena ju7847 
  * @author Jake Madlem     jxm9019
  * CS For Transfers
- * Project2
+ * Project3
  *
  * An interface puzzle class that contains stubs for a puzzle.
  */
