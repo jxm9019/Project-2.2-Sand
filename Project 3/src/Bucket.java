613a3ad4b8c58852d657bff547cdedd775ff9588
jxm9019 <jxm9019@cs.rit.edu>
2015-11-13 20:24:55 -0500

    made bucket

---
+++ Project 3/src/Bucket.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jacob on 11/13/2015.
+ */
+public class Bucket {
+    public static final int MINSAND = 0;
+    private int maxSand;
+    private int curSand;
+
+}
---
+++ Project 3/src/Sand.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Jacob Madlem on 11/13/2015.    jxm9019
+ *            Jean Luis Urnea on 11/13/2015. ju7847
+ */
+public class Sand {
+    public static final int MINSAND = 0;
+    private int maxSand;
+    private int curSand;
+
+
+
+
+
+}
