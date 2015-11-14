19ecbea156d0b8665253d9fec6bc65ff5399e349
jxm9019 <jxm9019@cs.rit.edu>
2015-11-13 20:43:31 -0500

    Bucket skeleton

--- Project 3/src/Bucket.java
+++ Project 3/src/Bucket.java
@@ -6,4 +6,50 @@ public class Bucket {
     private int maxSand;
     private int curSand;
 
+    public Bucket(int maxSand){
+        this.maxSand = maxSand;
+        this.curSand = 0;
+    }
+
+    public Bucket(Bucket bucket){
+        this.maxSand = bucket.maxSand;
+        this.curSand = bucket.curSand;
+    }
+
+    public int getCurSand() {
+        return curSand;
+    }
+
+    public int getMaxSand() {
+        return maxSand;
+    }
+
+    public void setMaxSand(int maxSand) {
+        this.maxSand = maxSand;
+    }
+
+    public void setCurSand(int curSand) {
+        this.curSand = curSand;
+    }
+
+    public boolean isFull(Bucket bucket){
+        return(bucket.curSand==bucket.maxSand);
+    }
+
+    public boolean isEmpty(Bucket bucket){
+        return(bucket.curSand == 0);
+    }
+
+    public void emptyBucket(Bucket bucket){
+        bucket.setCurSand(0);
+        System.out.println("Bucket is now empty.");
+    }
+
+    public void fill(Bucket bucket){
+        bucket.setCurSand(maxSand);
+    }
+
+
+
+
 }
--- Project 3/src/Sand.java
+++ Project 3/src/Sand.java
@@ -3,12 +3,5 @@
  *            Jean Luis Urnea on 11/13/2015. ju7847
  */
 public class Sand {
-    public static final int MINSAND = 0;
-    private int maxSand;
-    private int curSand;
-
-
-
-
 
 }
